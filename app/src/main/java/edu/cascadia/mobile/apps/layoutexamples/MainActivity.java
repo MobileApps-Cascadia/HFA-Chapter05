0bc36967c4536a91df7fc4680489d10dd8dffa0b
Charles Sy <charles.m.sy@hotmail.com>
2019-10-08 19:47:20 -0700

    Checkbox functioning spinner functioning button added toast added
    but they are not all connected together yet.

--- app/src/main/java/edu/cascadia/mobile/apps/layoutexamples/MainActivity.java
+++ app/src/main/java/edu/cascadia/mobile/apps/layoutexamples/MainActivity.java
@@ -2,12 +2,40 @@ package edu.cascadia.mobile.apps.layoutexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonToMakeTea;
+    CharSequence toastText = "Test"; // what the toast says
+    CheckBox checkBoxMilk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonToMakeTea =(Button)findViewById(R.id.makeTea);
+        checkBoxMilk = (CheckBox) findViewById(R.id.milkBox);
+
+        buttonToMakeTea.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                toastText = toastText.toString() + " Milk";
+                teaTea();
+            }
+        });
+    }
+
+    public void teaTea()
+    {
+
+        int duration = Toast.LENGTH_SHORT; // length of time toast is display
+
+        Toast toast = Toast.makeText(this, toastText, duration);
+        toast.show();
     }
 }
