8ede9bcbc9a8692ca4f793910210449c0171dea3
Clay Suttner <csuttner@gmail.com>
2019-10-10 19:04:05 -0700

    Trying to figure out the background image display issue

--- app/src/main/java/edu/cascadia/mobile/apps/layoutexamples/MainActivity.java
+++ app/src/main/java/edu/cascadia/mobile/apps/layoutexamples/MainActivity.java
@@ -1,13 +1,55 @@
 package edu.cascadia.mobile.apps.layoutexamples;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button tButton = (Button) findViewById(R.id.buttonToast);
+
+        tButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                showToast();
+            }
+        });
+    }
+
+    private void showToast(){
+        CheckBox milk = (CheckBox) findViewById(R.id.milkBox);
+        CheckBox sugar = (CheckBox) findViewById(R.id.sugarBox);
+        CheckBox lemon = (CheckBox) findViewById(R.id.lemonBox);
+
+        Spinner teaType = (Spinner) findViewById(R.id.teaType);
+
+        String selection = teaType.getSelectedItem().toString();
+
+        int duration = Toast.LENGTH_SHORT;
+
+        StringBuilder output = new StringBuilder("Tea: " + selection + " with");
+
+        if (milk.isChecked()){
+            output.append(" milk,");
+        }
+
+        if (sugar.isChecked()){
+            output.append(" sugar,");
+        }
+
+        if (lemon.isChecked()){
+            output.append(" lemon,");
+        }
+
+        Toast toast = Toast.makeText(this, output, duration);
+        toast.show();
     }
 }
