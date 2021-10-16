e890da505ab6ff6056b6d94e1498a20c46362ef0
Shahin and Issac <shahinnezam@hotmail.com>
2021-10-15 21:20:14 -0700

    Updated constraint and onClick (can't get toast to work - help!)

--- app/src/main/java/edu/cascadia/mobile/apps/layoutexamples/MainActivity.java
+++ app/src/main/java/edu/cascadia/mobile/apps/layoutexamples/MainActivity.java
@@ -3,6 +3,7 @@ package edu.cascadia.mobile.apps.layoutexamples;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
@@ -21,23 +22,27 @@ public class MainActivity extends AppCompatActivity {
         CheckBox lemonCheck = findViewById(R.id.lemonBox);
 
         //TODO: Assign each checkbox an onClick EventHandler that calls the onCheckboxClicked method
-
+        milkCheck.setOnClickListener(v -> onCheckboxClicked(v));
+        sugarCheck.setOnClickListener(v -> onCheckboxClicked(v));
+        lemonCheck.setOnClickListener(v -> onCheckboxClicked(v));
     }
 
     public void onCheckboxClicked(View view) {
-        CheckBox clicked = (CheckBox)view;
+        CheckBox clicked = (CheckBox) view;
         //Add or remove the checkboxes label to the ArrayList
-        if(clicked.isChecked())
+        if (clicked.isChecked())
             TeaGoodies.add(clicked.getText());
         else
             TeaGoodies.remove(clicked.getText());
 
         //Create a String of the current set of labels
         StringBuilder teaTime = new StringBuilder("Tea with ");
-        for (CharSequence goodies :TeaGoodies) {
-            teaTime.append(goodies).append((", "));
-        }
+        for (CharSequence goodies : TeaGoodies) teaTime.append(goodies).append((", "));
         //Display the string as a temporary popup - called a "Toast"
         Toast.makeText(MainActivity.this, teaTime, Toast.LENGTH_SHORT).show();
     }
-}
+        public void onClick(View view) {
+            onCheckboxClicked(view);
+        }
+    }
+
